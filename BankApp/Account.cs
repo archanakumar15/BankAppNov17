8fa6bb4fcc8f4d50350698616459e5105e5910a6
ARCHANA KUMAR <archanakumar.kn@gmail.com>
2017-11-18 12:49:54 -0800

    static/constructors/overloading

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -6,16 +6,35 @@ using System.Threading.Tasks;
 
 namespace BankApp
 {
+
+    enum TypeOfAccount
+    {
+        Checking,
+        Saving,
+        Cd,
+        Loan
+    }
     /// <summary>
     /// This is about bank account
     /// </summary>
     class Account
     {
+        #region Statics
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+
         #region Properties
         public int AccountNumber { get; private set; }
         public string EmailAddress { get; set; }
         public string AccountName { get; set; }
-        public string AccountType { get; set; }
+        public TypeOfAccount AccountType { get; set; }
         public decimal Balance { get; private set; }
         #endregion
 
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -11,12 +11,29 @@ namespace BankApp
         static void Main(string[] args)
         {
             //reference,object,instance of a class
-            var account = new Account();
-            account.EmailAddress = "test@test.com";
-            account.AccountType = "Checking";
+            var account = new Account
+            {
+                AccountName="My checking",
+                AccountType=TypeOfAccount.Checking,
+                EmailAddress="test@test.com"
+            };
+            
             account.Deposit(100.10M);
-            Console.WriteLine($"{account.Balance}");
+            Console.WriteLine($"AccountNumber:{account.AccountNumber}, Email Address:{account.EmailAddress}, Balance:{account.Balance}, Account Type:{account.AccountType}");//inline expression(if it start with $ then it is inline expression)
 
+            
+            var account2 = new Account
+            {
+                EmailAddress = "test2@test.com",
+                AccountType = TypeOfAccount.Checking,
+            };
+
+            
+            account2.Deposit(100.10M);
+            Console.WriteLine($"AccountNumber:{account2.AccountNumber}, Email Address:{account2.EmailAddress}, Balance:{account2.Balance}, " +
+                $"Account Type:{account2.AccountType}");//inline expression(if it start with $ then it is inline expression)
+            
 
         }
     }
+}
